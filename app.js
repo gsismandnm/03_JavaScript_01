e3613c19858353681fdd4aab2dec174880ac4360
Gökan ŞİŞMAN <gknsismandenmark@github.com>
2021-02-07 16:34:30 +0200

    Some Notes

---
+++ app.js
@@ -0,0 +1,39 @@
+/*  console.log 'Merhaba Kursiyerler'
+
+    var data =12;
+    var mesaj ="Hos geldiniz."
+    data =34;
+    Console ekrana yazdırmak için göstermek için kullanılır.
+    console.log("Değişkenimizin Değeri =" + data + " " + mesaj)
+    iki string değeri toplarkende matematik işlemlerde de + işareti kullanılıyor.
+*/
+
+
+/*  javascript büyük küçük harf duyarlılığı vardır.
+    console yazarken mutlaka ne yazdıysanız onu yazmanız lamız.
+    var name= "Murat";
+    console.log (name);
+    
+    CAMEL CASE yazılım kuralı. ilk harf küçük sonraki kelimeler büyük
+    ÖRNEK : var fetchUserNameFromService
+*/
+
+/* ARİTMATİK OPERATÖRLER : 
+    +   TOPLAMA
+    -   ÇIKARMA
+    /   BÖLME
+    *   ÇARPMA
+*/
+
+
+/*KARŞILAŞTIRMA OPERATÖRLERİ:
+    ==  EŞİT İSE
+    === VERİ TİPİ DAHİL EŞİT İSE
+    !=  EŞİT DEĞİL İSE 
+    !== VERİ TİPİ DAHİL EŞİT DEĞİLSE
+    >   BÜYÜKTÜR
+    <   KÜÇÜKTÜR
+    >=  BÜYÜK EŞİT
+    <=  KÜÇÜK EŞİT
+*/
+
